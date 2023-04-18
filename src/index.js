53a2fc15c90c17bde21988799a83aa9b11230f35
mecastor <mecastor87@gmail.com>
2023-04-18 19:35:21 +0100

    building src folder from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createFactory } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="book-list">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img src="./images/amazon-book1.jpg" alt="Bored of Lunch Book Cover" />
+  );
+};
+
+const Title = () => {
+  return (
+    <h2>Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER</h2>
+  );
+};
+
+const Author = () => {
+  return <h3>Nathan Anthony</h3>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
