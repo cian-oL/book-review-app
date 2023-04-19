b14c27c4e2aca2586a798468a038ae563d19a965
mecastor <mecastor87@gmail.com>
2023-04-19 11:34:10 +0100

    Refactored for props & used children prop

--- src/index.js
+++ src/index.js
@@ -3,28 +3,44 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const title = "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER";
-const author = "Nathan Anthony";
-const image = "./images/amazon-book1.jpg";
+const firstBook = {
+  title: "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER",
+  author: "Nathan Anthony",
+  image: "./images/amazon-book1.jpg",
+};
+
+const secondBook = {
+  title: "Lessons in Chemistry",
+  author: "Bonnie Garmus",
+  image: "./images/amazon-book2.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="book-list">
-      <Book author={author} title={title} image={image} />
-      <Book author={author} title={title} image={image} />
-      <Book author={author} title={title} image={image} />
-      <Book author={author} title={title} image={image} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        image={firstBook.image}
+      >
+      <p>Yadda yadda yadda</p>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        image={secondBook.image}
+      />
     </section>
   );
 };
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ author, title, image, children }) => {
   return (
     <article className="book">
-      <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h3>{props.author}</h3>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <p>{children}</p>
     </article>
   );
 };
