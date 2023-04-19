449ab11997b56e7cb731dccd650f9ed97117ca89
mecastor <mecastor87@gmail.com>
2023-04-19 12:10:19 +0100

    books passed as objects into render

--- src/index.js
+++ src/index.js
@@ -3,44 +3,37 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  title: "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER",
-  author: "Nathan Anthony",
-  image: "./images/amazon-book1.jpg",
-};
-
-const secondBook = {
-  title: "Lessons in Chemistry",
-  author: "Bonnie Garmus",
-  image: "./images/amazon-book2.jpg",
-};
+const books = [
+  {
+    title: "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER",
+    author: "Nathan Anthony",
+    image: "./images/amazon-book1.jpg",
+    id: 1,
+  },
+  {
+    title: "Lessons in Chemistry",
+    author: "Bonnie Garmus",
+    image: "./images/amazon-book2.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="book-list">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        image={firstBook.image}
-      >
-      <p>Yadda yadda yadda</p>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        image={secondBook.image}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-const Book = ({ author, title, image, children }) => {
+const Book = ({ author, title, image }) => {
   return (
     <article className="book">
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <h3>{author}</h3>
-      <p>{children}</p>
     </article>
   );
 };
