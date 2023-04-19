6235d191d7c48a3cebe2b3d8d673d48b6c34635b
mecastor <mecastor87@gmail.com>
2023-04-19 01:11:05 +0100

    refactored to introduce props

--- src/index.js
+++ src/index.js
@@ -3,29 +3,28 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const title = "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER";
+const author = "Nathan Anthony";
+const image = "./images/amazon-book1.jpg";
+
 const BookList = () => {
   return (
     <section className="book-list">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
     </section>
   );
 };
 
-const Book = () => {
-  const title = "Bored of Lunch: The Healthy Air Fryer Book: THE NO.1 BESTSELLER";
-  const author = "Nathan Anthony";
-
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="./images/amazon-book1.jpg"
-        alt="Bored of Lunch Book Cover"
-      />
-      <h2>{title}</h2>
-      <h3>{author}</h3>
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h3>{props.author}</h3>
     </article>
   );
 };
